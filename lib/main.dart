1968eedda29c6f6b19d112a542af939b941674f6
Danish <danishmaulahasbi@gmail.com>
2022-08-30 09:41:06 +0700

    make home page separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:exercise_switch/pages/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,20 +17,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-    );
-  }
-}
